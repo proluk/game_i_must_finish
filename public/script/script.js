555fa380ca74556c0c00c9feb165dc3458f8dda2
proluk <lukaszprokopiuk@gmail.com>
2017-01-08 16:29:59 +0100

    A lot of improvements finished. Open site, connect to other user, close site, disconnect.

--- public/script/script.js
+++ public/script/script.js
@@ -1,12 +1,18 @@
 let socket = io.connect('http://localhost:8000');
 
 $(document).ready(function(){
-	
+	let website = null;
 	let input = $('#input');
 	let log = $("#log");
 
 	socket.on("communicate", function(data) {
 		add(data.data);
+	});
+	socket.on("open", function(data){
+		website = window.open(data.data);
+	});
+	socket.on("close", function(){
+		website.close();
 	});
 
 	$("html").click(function() {
@@ -16,6 +22,7 @@ $(document).ready(function(){
 	    if(e.which == 13) {
 	        text = input.val();
 	        input.val("");
+	        add("~$ "+text);
 	        socket.emit("command", {command: text});
 	    }
 	});
--- server.js
+++ server.js
@@ -3,14 +3,24 @@ let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let path = require('path');
+let urlExists = require('url-exists');
 
 const communicates = {
 	begin : "Hello",
+	tip_01 : "",
+	tip_02 : "",
 	no_command : "No such command: ",
 	connected : "Connected to: ",
-	closed : "Connection Closed.",
+	disconnect : "You have been disconnected. ",
+	already_connected : "You are already connected somewhere.",
 	no_connection : "No connection to close.",
-	already_connected : "You are already connected somewhere."
+	open : "Open success, connected to: ",
+	no_website : "Cannot connect. No such website: ",
+	closed : "Connection Closed.",	
+	user_connected : "Unknown user has established a connection with you.",
+	close_site_first : "Close connection to site first.",
+	disconnect_user_first : "Disconnect from user first.",
+	user_left : "Uknown user has left."
 }
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -23,35 +33,89 @@ let index = require(path.join(__dirname, '/routes/index.js'));
 app.use('/',index);
 
 io.on('connection', function(socket) {
-	console.log('socket connected');
+	socket.join(socket.id);
+	let home = socket.id;
+	let connection = false;
 	let site = false;
 	let commands = {
-		connect : function(link){
-			if ( site ) {
+		connect : function(user, blank){
+			if ( connection ) {
 				socket.emit("communicate", {data: communicates.already_connected});
+			} else if ( site ) {
+				socket.emit("communicate", {data: communicates.close_site_first});
 			} else {
-				let adjusted_link = verifyLink(link);
-				if(adjusted_link) {
-					site = adjusted_link;
-					socket.emit("communicate", {data: communicates.connected+adjusted_link});	
-					socket.join(adjusted_link);								
+				connection = user;
+				socket.join(connection);
+				io.sockets.connected[connection].emit("communicate", {data: communicates.user_connected});
+				socket.emit("communicate", {data: communicates.connected+connection});									
+			}
+		},
+		disconnect : function(who, blank) {
+			if ( site ) {
+				socket.emit("communicate", {data: communicates.close_site_first});
+			} else if ( connection && !who ) {
+				socket.leave(connection);
+				socket.broadcast.to(connection).emit("communicate", {data: communicates.user_left});
+				connection = false;
+			} else if ( connection && who ) {
+				if (io.sockets.connected[who]) {
+			    	io.sockets.connected[who].leave(connection);
+			    	io.sockets.connected[who].emit("communicate", {data: communicates.disconnect});
+				}
+			} else if ( !connection && !who ) {
+				//wyrzuca siebie z domu
+
+			} else if ( !connection && who ) {
+				if (io.sockets.connected[who]) {
+			    	io.sockets.connected[who].leave(home);
+			    	io.sockets.connected[who].emit("communicate", {data: communicates.disconnect});
 				}
 			}
 		},
-		close : function(blank){
+		open : function ( link , blank ) {
+			if ( site ) {
+				socket.emit("communicate", {data: communicates.already_connected});
+			} else if ( connection ) {
+				socket.emit("communicate", {data: communicates.disconnect_user_first});
+			} else {
+				urlExists(link, function(err, exists) {
+					if ( exists ) {
+						site = link;
+						socket.join(link);
+						socket.emit("open", {data: link});
+						socket.emit("communicate", {data: communicates.open+link});
+					} else {
+						socket.emit("communicate", {data: communicates.no_website+link});
+					}
+				});
+			}
+		},
+		close : function(blank, blank){
 			if ( site ) {
 				socket.emit("communicate", {data: communicates.closed});
 				socket.leave(site);
+				socket.emit("close");
 				site = false;
 			} else {
 				socket.emit("communicate", {data: communicates.no_connection});
 			}
 		},
-		show : function(blank) {
-			let inrooms = io.sockets.clients("https://facebook.com");
-			for ( let i = 0 ; i < inrooms.length ; i ++ ) {
-				console.log("test");
-				socket.emit("communicate", {data: i+" : "+inrooms[i]});
+		show : function(blank, blank) {
+			if( site ) {
+				let inrooms = io.sockets.adapter.rooms[site];
+				for ( let sockete in inrooms.sockets ) {
+					socket.emit("communicate", {data: sockete});
+				}
+			} else if ( connection ) {
+				let inrooms = io.sockets.adapter.rooms[connection];
+				for ( let sockete in inrooms.sockets ) {
+					socket.emit("communicate", {data: sockete});
+				}
+			} else {
+				let inrooms = io.sockets.adapter.rooms[home];
+				for ( let sockete in inrooms.sockets ) {
+					socket.emit("communicate", {data: sockete});
+				}
 			}
 		}
 	}
@@ -62,18 +126,13 @@ io.on('connection', function(socket) {
 	socket.on("command", function(data) {
 		veryfiCommand(data);
 	});
+	socket.on("disconnect", function(socket) {
+		console.log("socket disconnected");
+	});
 
 	function  veryfiCommand(data) {
 		command = data.command.split(" ");
 		commands[command[0]] ? commands[command[0]](command[1]) : socket.emit("communicate", {data: communicates.no_command+command});
-	}
-	function verifyLink(link) {
-		let string = link.toLowerCase().replace("http", "https").replace("www.","https://"); 
-		if(string.indexOf("https")===0){
-			return string;
-		} else {
-			return false;
-		}
 	}
 });
 
