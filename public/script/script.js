6a08527db23b647e8e5ecee1c3a8f8de281ecb5c
Luku Laptop <lukaszprokopiuk@gmail.com>
2017-01-08 12:36:35 +0100

    Not done, commiting to and at station

--- public/script/script.js
+++ public/script/script.js
@@ -1,16 +1,26 @@
+let socket = io.connect('http://localhost:8000');
+
 $(document).ready(function(){
+	
 	let input = $('#input');
 	let log = $("#log");
-	
+
+	socket.on("communicate", function(data) {
+		add(data.data);
+	});
+
 	$("html").click(function() {
         input.val(input.val()).focus();
     });       
     $(document).keypress(function(e) {
 	    if(e.which == 13) {
 	        text = input.val();
-	        log.append("<div class='row'>"+text+"</div>");
 	        input.val("");
-	        window.scrollTo(0,document.body.scrollHeight);
+	        socket.emit("command", {command: text});
 	    }
 	});
+	function add(data){
+		log.append("<div class='row'>"+data+"</div>");
+		window.scrollTo(0,document.body.scrollHeight);
+	}
 });
--- server.js
+++ server.js
@@ -4,6 +4,15 @@ let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let path = require('path');
 
+const communicates = {
+	begin : "Hello",
+	no_command : "No such command: ",
+	connected : "Connected to: ",
+	closed : "Connection Closed.",
+	no_connection : "No connection to close.",
+	already_connected : "You are already connected somewhere."
+}
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('views', path.join(__dirname, '/views'));
@@ -15,6 +24,57 @@ app.use('/',index);
 
 io.on('connection', function(socket) {
 	console.log('socket connected');
+	let site = false;
+	let commands = {
+		connect : function(link){
+			if ( site ) {
+				socket.emit("communicate", {data: communicates.already_connected});
+			} else {
+				let adjusted_link = verifyLink(link);
+				if(adjusted_link) {
+					site = adjusted_link;
+					socket.emit("communicate", {data: communicates.connected+adjusted_link});	
+					socket.join(adjusted_link);								
+				}
+			}
+		},
+		close : function(blank){
+			if ( site ) {
+				socket.emit("communicate", {data: communicates.closed});
+				socket.leave(site);
+				site = false;
+			} else {
+				socket.emit("communicate", {data: communicates.no_connection});
+			}
+		},
+		show : function(blank) {
+			let inrooms = io.sockets.clients("https://facebook.com");
+			for ( let i = 0 ; i < inrooms.length ; i ++ ) {
+				console.log("test");
+				socket.emit("communicate", {data: i+" : "+inrooms[i]});
+			}
+		}
+	}
+	let command = [];
+
+	socket.emit("communicate", {data: communicates.begin});
+
+	socket.on("command", function(data) {
+		veryfiCommand(data);
+	});
+
+	function  veryfiCommand(data) {
+		command = data.command.split(" ");
+		commands[command[0]] ? commands[command[0]](command[1]) : socket.emit("communicate", {data: communicates.no_command+command});
+	}
+	function verifyLink(link) {
+		let string = link.toLowerCase().replace("http", "https").replace("www.","https://"); 
+		if(string.indexOf("https")===0){
+			return string;
+		} else {
+			return false;
+		}
+	}
 });
 
 server.listen(8000, function(){
